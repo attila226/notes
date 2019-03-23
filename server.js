37baf7378e7632b29fe4286ef9a7a51931485075
Attila Buturla <AttilaButurla@gmail.com>
2019-03-22 22:58:37 -0700

    Added Webpack dev server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const apiRoutes = require('./api/routes');
 
 app.listen(8080, () => {
@@ -7,8 +8,8 @@ app.listen(8080, () => {
 });
 
 app.use(express.json());
-
 app.use(express.static('public'));
+app.use(cors())
 
 //API endpoints
 app.use(apiRoutes);
